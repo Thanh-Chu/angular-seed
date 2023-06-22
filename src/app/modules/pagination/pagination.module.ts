788fe5b458f791acf94d83514b0c7e5e76b08612
Vuong Pham <phamvanvuong2351995@gmail.com>
2023-06-22 09:14:52 +0700

    Create-Pagination

---
+++ src/app/modules/pagination/pagination.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PaginationRoutingModule } from './pagination-routing.module';
+import { PaginationComponent } from './pagination/pagination.component';
+
+@NgModule({
+  declarations: [
+    PaginationComponent
+  ],
+  imports: [CommonModule, PaginationRoutingModule],
+})
+export class PaginationModule {}
